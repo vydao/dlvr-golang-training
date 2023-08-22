dcc3ef874e051797232e7f3ab95607a88d86cd1f
Vy Dao <vynguyendao@gmail.com>
2023-08-22 11:06:40 +0700

    Use io.Copy instead of io.ReadAll

--- tutorial-1/ch1/fetch/main.go
+++ tutorial-1/ch1/fetch/main.go
@@ -14,12 +14,17 @@ func main() {
 			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
 			os.Exit(1)
 		}
-		b, err := io.ReadAll(resp.Body)
+		// b, err := io.ReadAll(resp.Body)
+		io.Copy(os.Stdout, resp.Body)
 		resp.Body.Close()
 		if err != nil {
-			fmt.Fprintf(os.Stderr, "fetch: reading: %s: %v\n", url, err)
+			fmt.Fprintf(os.Stderr, "fetch: copy error: %v\n", err)
 			os.Exit(1)
 		}
-		fmt.Printf("%s", b)
+		// if err != nil {
+		// 	fmt.Fprintf(os.Stderr, "fetch: reading: %s: %v\n", url, err)
+		// 	os.Exit(1)
+		// }
+		// fmt.Printf("%s", b)
 	}
 }
